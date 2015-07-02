522df644530a7ff63cd832b013b0621791d0eaf5
RelacodeTech <relacode.tech@gmail.com>
2015-07-02 18:46:21 +0530

    First commit

---
+++ SpaEFV3.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpaEFV3.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SpaEFV3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaEFV3.MvcApplication" Language="C#" %>
