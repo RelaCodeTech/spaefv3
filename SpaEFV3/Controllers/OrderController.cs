bf337e3c263c8b455e4d22f1955d9bd0d20d0977
RelacodeTech <relacode.tech@gmail.com>
2015-07-03 18:26:08 +0530

    added - order ,voucher

---
+++ SpaEFV3/Controllers/OrderController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpaEFV3.Models;
+
+namespace SpaEFV3.Controllers
+{
+    public class OrderController : Controller
+    {
+        private DevSpaEntities db = new DevSpaEntities();
+
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Customer).Include(o => o.Location);
+            return View(orders.ToList());
+        }
+
+        // GET: /Order/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: /Order/Create
+        public ActionResult Create()
+        {
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Fname");
+            ViewBag.Order_Loc_ID = new SelectList(db.Locations, "Loc_ID", "Loc_Code");
+            return View();
+        }
+
+        // POST: /Order/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Order_ID,Customer_ID,Order_Dt,Order_Loc_ID,Order_Currency_Code,Order_Gross_Amount,Order_Net_Amount,Voucher_Usage_ID,Order_Comments,Order_Created_By,Order_Updated_Dt,Order_Updated_By")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Fname", order.Customer_ID);
+            ViewBag.Order_Loc_ID = new SelectList(db.Locations, "Loc_ID", "Loc_Code", order.Order_Loc_ID);
+            return View(order);
+        }
+
+        // GET: /Order/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Fname", order.Customer_ID);
+            ViewBag.Order_Loc_ID = new SelectList(db.Locations, "Loc_ID", "Loc_Code", order.Order_Loc_ID);
+            return View(order);
+        }
+
+        // POST: /Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Order_ID,Customer_ID,Order_Dt,Order_Loc_ID,Order_Currency_Code,Order_Gross_Amount,Order_Net_Amount,Voucher_Usage_ID,Order_Comments,Order_Created_By,Order_Updated_Dt,Order_Updated_By")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Customer_ID = new SelectList(db.Customers, "Customer_ID", "Customer_Fname", order.Customer_ID);
+            ViewBag.Order_Loc_ID = new SelectList(db.Locations, "Loc_ID", "Loc_Code", order.Order_Loc_ID);
+            return View(order);
+        }
+
+        // GET: /Order/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SpaEFV3/Controllers/VoucherController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpaEFV3.Models;
+
+namespace SpaEFV3.Controllers
+{
+    public class VoucherController : Controller
+    {
+        private DevSpaEntities db = new DevSpaEntities();
+
+        // GET: /Voucher/
+        public ActionResult Index()
+        {
+            return View(db.Voucher_Master.ToList());
+        }
+
+        // GET: /Voucher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voucher_Master voucher_master = db.Voucher_Master.Find(id);
+            if (voucher_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(voucher_master);
+        }
+
+        // GET: /Voucher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Voucher/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Voucher_ID,Voucher_Amount,Voucher_Created_Dt,Voucher_Expiry_Dt,Voucher_Created_By")] Voucher_Master voucher_master)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Voucher_Master.Add(voucher_master);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(voucher_master);
+        }
+
+        // GET: /Voucher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voucher_Master voucher_master = db.Voucher_Master.Find(id);
+            if (voucher_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(voucher_master);
+        }
+
+        // POST: /Voucher/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Voucher_ID,Voucher_Amount,Voucher_Created_Dt,Voucher_Expiry_Dt,Voucher_Created_By")] Voucher_Master voucher_master)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(voucher_master).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(voucher_master);
+        }
+
+        // GET: /Voucher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Voucher_Master voucher_master = db.Voucher_Master.Find(id);
+            if (voucher_master == null)
+            {
+                return HttpNotFound();
+            }
+            return View(voucher_master);
+        }
+
+        // POST: /Voucher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Voucher_Master voucher_master = db.Voucher_Master.Find(id);
+            db.Voucher_Master.Remove(voucher_master);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
