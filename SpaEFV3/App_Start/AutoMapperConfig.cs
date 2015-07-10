300271b2c12d25222489bfea6cecc87a79ffa1f8
RelacodeTech <relacode.tech@gmail.com>
2015-07-10 19:55:27 +0530

    Latest-DB-Scripts

---
+++ SpaEFV3/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SpaEFV3.Models;
+using SpaEFV3.ViewModels.TaxMaster;
+
+namespace SpaEFV3.App_Start
+{
+    /// <summary>
+    /// AutoMapper mappings for classes
+    /// </summary>
+    public static class AutoMapperConfig
+    {
+        /// <summary>
+        /// Add all mappings here
+        /// </summary>
+        public static void RegisterMappings()
+        {
+            //mapping -> TaxMaster.LookUp_Country.Country_Name to TaxMasterListViewModel.CountryName
+            AutoMapper.Mapper.CreateMap<Tax_Master, TaxMasterListViewModel>()
+                .ForMember(dest => dest.CountryName,
+                opts => opts.MapFrom(src => src.LookUp_Country.Country_Name));
+        }
+    }
+}
